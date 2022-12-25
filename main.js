dd51e5787e42960f57b5356027129e1e879b4c23
foo bar <86317585+lel64codes@users.noreply.github.com>
2022-12-25 08:24:05 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,46 @@
+const MessageBox = document.getElementById("CurrentMessage");
+const AddMessagesHere = document.getElementById("MessagesWrapper");
+AssistanceBOT.Activate();
+function SendText(Username = "Likely_A_Human", Message = MessageBox.value) {
+  var NewElem = document.createElement("div");
+  var UserNameElem = document.createElement("strong");
+  var InnerNewElem = document.createElement("p");
+  UserNameElem.textContent = Username + ": ";
+  InnerNewElem.textContent = Message;
+  NewElem.className = "Message";
+  NewElem.appendChild(UserNameElem);
+  NewElem.appendChild(InnerNewElem);
+  AddMessagesHere.appendChild(NewElem);
+  AddMessagesHere.appendChild(document.createElement("hr"));
+  console.log(Message)
+  if (Message.toLowerCase() === "help") {
+    AssistanceBOT.Activate();
+  }
+  else if (Message.toLowerCase() === "spam") {
+    SpammerBOT.Activate();
+  }
+}
+function SendHTML(Username = "Likely_A_Human", Message = MessageBox.value) {
+  var NewElem = document.createElement("div");
+  var UserNameElem = document.createElement("strong");
+  var InnerNewElem = document.createElement("p");
+  UserNameElem.textContent = Username + ": ";
+  InnerNewElem.innerHTML = Message;
+  NewElem.className = "Message";
+  NewElem.appendChild(UserNameElem);
+  NewElem.appendChild(InnerNewElem);
+  AddMessagesHere.appendChild(NewElem);
+  AddMessagesHere.appendChild(document.createElement("hr"));
+}
+function SendJS(Code = MessageBox.value) {
+  try {
+    alert("Eval results: " + eval(Code));
+  }
+  catch {
+    alert("JS Error");
+    return;
+  }
+}
+function Clear() {
+  MessageBox.value = "";
+}
